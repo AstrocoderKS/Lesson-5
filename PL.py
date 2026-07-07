7bee47f9a9b1b5675b7db72e24cc9e29f8e3e7b2
AstrocoderKS <astroks26@gmail.com>
2026-07-07 19:32:15 +1000

    LP

---
+++ PL.py
@@ -0,0 +1,6 @@
+A = int(input("Enter the price for the product."))
+S = int(input("Enter the selling price for the product."))
+if A>=S:
+    print("This is a loss.")
+else:
+    print("This is a profit.")
